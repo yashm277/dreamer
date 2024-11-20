e8782b913ed027ab2fa0a07aa6123f97733c8da8
yashm277 <f20210001@pilani.bits-pilani.ac.in>
2024-11-20 22:30:37 +0000

    starting

---
+++ src/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+        <h1>The scrapbook for the dreamers who'll never run out of ideas</h1>
+        <button>Get started</button>
+    </div>
+  )
+}
+
+export default Homepage
